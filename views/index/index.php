63b9dac65f2fc11ee407a949a70ed6873d34e7cb
daysystem <mmovs1386@gmail.com>
2022-07-23 15:14:40 +0430

    services_ui

--- views/index/index.php
+++ views/index/index.php
@@ -2,10 +2,12 @@
     <link rel="stylesheet" href="<?= URL ?>/public/css/index/index.css">
     <link rel="stylesheet" href="<?= URL ?>/public/css/index/sidebar.css">
     <link rel="stylesheet" href="<?= URL ?>/public/css/index/slider1.css">
+    <link rel="stylesheet" href="<?= URL ?>/public/css/index/services.css">
     <div class="main clearfix width_1190 margin_center">
         <?php require("banner.php") ?>
         <?php require("sidebar.php") ?>
         <?php require("slider1.php") ?>
+        <?php require("services.php") ?>
     </div>
     <script src="<?= URL ?>/public/js/slider1.js"></script>
 </main>
---
+++ views/index/services.php
@@ -0,0 +1,34 @@
+<div class="services bg_white default_border margin_bottom_10">
+    <ul class="max_width max_height">
+        <li class="max_height right pointer text_center">
+            <a class="block max_height red default_transition text_center relative">
+                <i class="red far fa-calendar-alt inline block max_height margin_left_10"></i>
+                <p class="inline relative">٧ روز ضمانت بازگشت</p>
+            </a>
+        </li>
+        <li class="max_height right pointer">
+            <a class="block max_height red default_transition text_center relative">
+                <i class="blue far fa-credit-card inline block max_height margin_left_10"></i>
+                <p class="inline relative">پرداخت در محل</p>
+            </a>
+        </li>
+        <li class="max_height right pointer">
+            <a class="block max_height red default_transition text_center relative">
+                <i class="green fas bold fa-check inline block max_height margin_left_10"></i>
+                <p class="inline relative">ضمانت اصل بودن کالا</p>
+            </a>
+        </li>
+        <li class="max_height right pointer">
+            <a class="block max_height red default_transition text_center relative">
+                <i class="orange far fa-trailer inline block max_height margin_left_10"></i>
+                <p class="inline relative">تحویل اکسپرس</p>
+            </a>
+        </li>
+        <li class="max_height right pointer">
+            <a class="block max_height red default_transition text_center relative">
+                <i class="pink far fa-dollar-circle inline block max_height margin_left_10"></i>
+                <p class="inline relative">تضمین بهترین قیمت</p>
+            </a>
+        </li>
+    </ul>
+</div>
